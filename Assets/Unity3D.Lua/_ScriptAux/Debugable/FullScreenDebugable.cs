c88e651ebf7228fd4b0b6805e51ab5624d48478c
0x600d1dea <xiaobin.huang@gmail.com>
2018-01-14 03:16:53 +0800

    move things to better place. add Debugable script

---
+++ Assets/Unity3D.Lua/_ScriptAux/Debugable/FullScreenDebugable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenDebugable : Debugable
+{
+	static Debugable instance_;
+	public static Debugable instance
+	{
+		get
+		{
+			if (instance_ == null)
+			{
+				instance_ = GameObject.FindObjectOfType<FullScreenDebugable>();
+			}
+			if (instance_ == null)
+			{
+				var go = new GameObject("_FullScreenDebugable");
+				DontDestroyOnLoad(go);
+				instance_ = go.AddComponent<FullScreenDebugable>();
+			}
+			return instance_;
+		}
+	}
+
+	void Awake()
+	{
+		Editor_SetArea(2 * Screen.width / 3, 0, Screen.width / 3, Screen.height);
+		show = true;
+		var f = lua.LuaFunction.CreateDelegate(_Init.luaVm, new System.Action(Editor_TogglePopUp));
+		Editor_AddToolbarButton("PopUp", f);
+		f.Dispose();
+	}
+
+}
+
---
+++ Assets/_ScriptLua/Boot.lua
@@ -0,0 +1,17 @@
+local Boot = {}
+
+local Color = csharp.checked_import('UnityEngine.Color')
+function Boot:Start()
+	local Debugable = csharp.checked_import('Debugable')
+	local dbg = self:GetComponent(Debugable)
+	dbg:Editor_ToggleGUI()
+	dbg:Editor_AddGraph(
+		'random', 'unit',
+		function()
+			return math.random()
+		end,
+		10, 0.5,
+		100, 100, 200, 100, Color.red)
+end
+
+return Boot
